49aa5975c5dac70589b5db4b01c1ab1500747ac7
rubyjane88 <rubyjane88@gmail.com>
2018-07-25 22:06:40 +0800

    Notify Property Changed added

---
+++ RestAPI_XF/RestAPI_XF/JobOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace RestAPI_XF
+{
+    public class JobOrder
+    {
+        public int Id { get; set; }
+
+        private string _serviceOrder;
+
+       
+        [JsonProperty("ServiceOrder")]
+        public string ServiceOrder
+        {
+            get => _serviceOrder;
+            set
+            {
+                _serviceOrder = value;
+        
+            }
+
+        }
+
+        
+    }
+}
--- RestAPI_XF/RestAPI_XF/MainPage.xaml.cs
+++ RestAPI_XF/RestAPI_XF/MainPage.xaml.cs
@@ -13,5 +13,10 @@ namespace RestAPI_XF
 		{
 			InitializeComponent();
 		}
+
+	    private void OnAdd(object sender, EventArgs e)
+	    {
+	        throw new NotImplementedException();
+	    }
 	}
 }
