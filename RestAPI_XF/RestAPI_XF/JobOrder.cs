4546c5feb49423334b7603b8334a91ab004b38c2
rubyjane88 <rubyjane88@gmail.com>
2018-07-26 08:39:17 +0800

    created observable collections

--- RestAPI_XF/RestAPI_XF/JobOrder.cs
+++ RestAPI_XF/RestAPI_XF/JobOrder.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using Newtonsoft.Json;
 
 namespace RestAPI_XF
 {
-    public class JobOrder
+    public class JobOrder : INotifyPropertyChanged
     {
         public int Id { get; set; }
 
         private string _serviceOrder;
 
-       
         [JsonProperty("ServiceOrder")]
         public string ServiceOrder
         {
             get => _serviceOrder;
-            set
-            {
-                _serviceOrder = value;
-        
-            }
-
+            set => _serviceOrder = value;
         }
 
-        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- RestAPI_XF/RestAPI_XF/MainPage.xaml.cs
+++ RestAPI_XF/RestAPI_XF/MainPage.xaml.cs
@@ -1,22 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace RestAPI_XF
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+
+	    private const string Url = "http://10.0.2.2:3000/joborders/"; //for Android emulators
+	    private readonly HttpClient _client = new HttpClient();
+	    private ObservableCollection<JobOrder> _orders;
+
+	    public MainPage()
 		{
 			InitializeComponent();
 		}
 
+	    protected override async void OnAppearing()
+	    {
+	       var content = await _client.GetStringAsync(Url);
+	       List<JobOrder> orders = JsonConvert.DeserializeObject<List<JobOrder>>(content);
+	        _orders  = new ObservableCollection<JobOrder>(orders);
+	        JobListView.ItemsSource = _orders; 
+            
+	        base.OnAppearing();
+	    }
+
 	    private void OnAdd(object sender, EventArgs e)
 	    {
-	        throw new NotImplementedException();
+	       
+	    }
+
+	    private void OnUpdate(object sender, EventArgs e)
+	    {
+	        
+	    }
+
+	    private void OnDelete(object sender, EventArgs e)
+	    {
+	       
 	    }
 	}
 }
