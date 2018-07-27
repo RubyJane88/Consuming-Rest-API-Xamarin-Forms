108ed041c7b4ec937a6f392c59683af6cbaf975b
rubyjane88 <rubyjane88@gmail.com>
2018-07-27 22:01:37 +0800

    http client and observable collections

--- RestAPI_XF/RestAPI_XF/JobOrder.cs
+++ RestAPI_XF/RestAPI_XF/JobOrder.cs
@@ -1,24 +1,31 @@
 ﻿using Newtonsoft.Json;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using RestAPI_XF.Annotations;
 
 namespace RestAPI_XF
 {
     public class JobOrder : INotifyPropertyChanged
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
 
         private string _serviceOrder;
 
-        [JsonProperty("ServiceOrder")]
+        [JsonProperty("serviceOrder")]
         public string ServiceOrder
         {
             get => _serviceOrder;
-            set => _serviceOrder = value;
+            set
+            {
+                _serviceOrder = value; 
+                OnPropertyChanged();
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- RestAPI_XF/RestAPI_XF/MainPage.xaml.cs
+++ RestAPI_XF/RestAPI_XF/MainPage.xaml.cs
@@ -1,50 +1,57 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Threading;
 using Xamarin.Forms;
 
 namespace RestAPI_XF
 {
-	public partial class MainPage : ContentPage
-	{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://10.0.2.2:3000/joborders/"; //for Android emulators
+        private readonly HttpClient _client = new HttpClient();
+        private ObservableCollection<JobOrder> _orders;
 
-	    private const string Url = "http://10.0.2.2:3000/joborders/"; //for Android emulators
-	    private readonly HttpClient _client = new HttpClient();
-	    private ObservableCollection<JobOrder> _orders;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-	    public MainPage()
-		{
-			InitializeComponent();
-		}
+        protected override async void OnAppearing()
+        {
+            var content = await _client.GetStringAsync(Url);
+            List<JobOrder> orders = JsonConvert.DeserializeObject<List<JobOrder>>(content);
+            _orders = new ObservableCollection<JobOrder>(orders);
+            JobListView.ItemsSource = _orders;
 
-	    protected override async void OnAppearing()
-	    {
-	       var content = await _client.GetStringAsync(Url);
-	       List<JobOrder> orders = JsonConvert.DeserializeObject<List<JobOrder>>(content);
-	        _orders  = new ObservableCollection<JobOrder>(orders);
-	        JobListView.ItemsSource = _orders; 
+            base.OnAppearing();
+        }
+
+        public async void OnAdd(object sender, EventArgs e)
+        {
+            JobOrder jobOrder = new JobOrder { ServiceOrder = $"Timestamp {DateTime.UtcNow.Ticks}" };
+            string content = JsonConvert.SerializeObject(jobOrder);
+            await _client.PostAsync(Url, new StringContent(content, Encoding.UTF8, "application/json"));
+            _orders.Insert(0, jobOrder);
+        }
+
+        public async void OnUpdate(object sender, EventArgs e)
+        {
+            JobOrder jobOrder = _orders[0];
+            jobOrder.ServiceOrder += "[updated]";
+            string content = JsonConvert.SerializeObject(jobOrder);
+            await _client.PutAsync(Url + jobOrder.Id, new StringContent(content, Encoding.UTF8, "application/json"));
             
-	        base.OnAppearing();
-	    }
+        }
 
-	    private void OnAdd(object sender, EventArgs e)
-	    {
-	       
-	    }
-
-	    private void OnUpdate(object sender, EventArgs e)
-	    {
-	        
-	    }
-
-	    private void OnDelete(object sender, EventArgs e)
-	    {
-	       
-	    }
-	}
+        public async void OnDelete(object sender, EventArgs e)
+        {
+            JobOrder jobOrder = _orders[0];
+            await _client.DeleteAsync(Url + jobOrder.Id);
+            _orders.Remove(jobOrder);
+        }
+    }
 }
